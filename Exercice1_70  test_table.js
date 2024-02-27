6044a9504b84833e5caa845a9a6b182c6c7b2de0
Kaiser-G <kaiserg6969@gmail.com>
2024-02-27 16:23:15 +0100

    Mise en place des 2 pages exo 1

---
+++ Exercice1_70  test_table.js
@@ -0,0 +1,23 @@
+class Table {
+  id_zone = "";
+  class_table = "";
+  data = "";
+  header = "";
+  class_modif = "";
+  class_supp = "";
+  class_vue = "";
+  icone_modif = "";
+  icone_vue = "";
+  icone_supp = "";
+  separateur = "";
+
+  // Les méthodes
+
+  //générer
+
+  //function_modif
+
+  //function_vue
+
+  //fonction_suppr
+}
