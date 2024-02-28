4e8809f3f45e494f631c21c653d931c289069d72
Kaiser-G <kaiserg6969@gmail.com>
2024-02-28 17:25:06 +0100

    Les boutons click reste a faire function

--- Exercice1_70  test_table.js
+++ Exercice1_70  test_table.js
@@ -9,7 +9,7 @@ class Table {
   icone_modif = "";
   icone_vue = "";
   icone_supp = "";
-  separateur = "";
+  separateur = "*";
 
   // Les méthodes
   fonction_modif = ""; // doit contenir la fonction qui sera appelée lors du
@@ -61,45 +61,70 @@ class Table {
           td.innerText = cellule;
           tr_body.appendChild(td);
         });
-        //création des boutons
-        //bouton vue
-        let td = document.createElement("td");
-        tr_body.appendChild(td);
-        let btn = document.createElement("button");
-        btn.className = "vueRegion btn btn-danger btn-sm fas fa-eye fa-sm";
-        td.appendChild(btn);
-        btn.addEventListener("click", (e) => {
-          alert("bouton vue");
-        });
 
-        //création modif
-        let btn2 = document.createElement("button");
-        btn2.className = "vueRegion btn btn-success btn-sm fas fa-pencil fa-sm";
-        td.appendChild(btn2);
-        btn2.addEventListener("click", (e) => {
-          alert("bouton modif");
-        });
+        //typeof
+        if (
+          typeof this.fonction_modif == "function" ||
+          typeof this.fonction_suppr == "function" ||
+          typeof this.fonction_vue == "function"
+        ) {
+          //création des boutons
+          //bouton vue
+          let td = document.createElement("td");
+          tr_body.appendChild(td);
 
-        //création suppr
-        let btn3 = document.createElement("button");
-        btn3.className = "vueRegion btn btn-info btn-sm fas fa-trash fa-sm";
-        td.appendChild(btn3);
-        btn3.addEventListener("click", (e) => {
-          alert("bouton supp");
-        });
+          let values = item.join(this.separateur);
+          // alert(values);
+          if (typeof this.fonction_vue == "function") {
+            let btn = document.createElement("button");
+            btn.className = this.icone_vue;
+            //btn.className = "vueRegion btn btn-danger btn-sm fas fa-eye fa-sm";
+            btn.value = values;
 
-        //création des boutons
-        //bouton vue
-        // if (this.fonction_vue == "fonction") {
-        //   let btn = document.createElement("button");
-        //   btn.className = "vueRegion btn btn-success btn-sm fas fa-eye fa-sm";
-        //   btn.addEventListener("click", (e) => {
-        //     alert("bouton vue");
-        //     td.appendChild(btn);
-        //   });
-        // }
+            btn.addEventListener(
+              "click",
+              (e) => {
+                this.fonction_vue(e);
+              },
+              false
+            );
+            td.appendChild(btn);
+          }
+
+          //création modif
+          if (typeof this.fonction_modif == "function") {
+            let btn2 = document.createElement("button");
+            btn2.className =
+              "vueRegion btn btn-success btn-sm fas fa-pencil fa-sm";
+            btn2.value = values;
+            btn2.addEventListener(
+              "click",
+              (e) => {
+                this.fonction_modif(e);
+              },
+              false
+            );
+            td.appendChild(btn2);
+          }
+
+          //création suppr
+          if (typeof this.fonction_suppr == "function") {
+            let btn3 = document.createElement("button");
+            btn3.className = "vueRegion btn btn-info btn-sm fas fa-trash fa-sm";
+            btn3.value = values;
+            btn3.addEventListener(
+              "click",
+              (e) => {
+                this.fonction_suppr(e);
+              },
+              false
+            );
+            td.appendChild(btn3);
+          }
+        }
       });
 
+      // document.getElementById(this.id_zone).appendChild(tab);
       zone.appendChild(tab);
     } else {
       throw new Error(
