f77845fae6a1d5e4361c7495ab4381d19ff1c50a
Kaiser-G <kaiserg6969@gmail.com>
2024-02-28 11:32:21 +0100

    On passe a la V2 pour test

--- Exercice1_70  test_table.js
+++ Exercice1_70  test_table.js
@@ -12,7 +12,6 @@ class Table {
   separateur = "";
 
   // Les méthodes
-  // Les méthodes
   fonction_modif = ""; // doit contenir la fonction qui sera appelée lors du
   // click sur le bouton MODIF
   fonction_suppr = ""; // doit contenir la fonction qui sera appelée lors du
@@ -26,28 +25,32 @@ class Table {
   generer() {
     if (this.id_zone != "") {
       let tab = document.createElement("table");
+      tab.className = this.class_table;
 
       //création du thead
       let thead = document.createElement("thead"); // un seul par tableau
+      thead = this.header;
       let tr = document.createElement("tr"); //que pour les lignes
-
+      let colonne = this.header;
       for (let i = 1; i <= colonne; i++) {
         let th = document.createElement("th"); //ligne 1 pour les titres en gras, colonne
-        th.innerText = "Entete" + i;
+        th = this.data.value[0];
+        th.innerText = "" + i;
         tr.appendChild(th);
         thead.appendChild(tr);
       }
       let th = document.createElement("th");
-      th.innerText = "Actions";
+      th.innerText = "";
       tr.appendChild(th);
-      tab.appendChild(thead);
+      // tab.appendChild(thead);
 
       //création du body
       let body = document.createElement("tbody"); //un seul par tableau qui contient :tr > td
       tab.appendChild(body);
-
+      let ligne = this.data;
       for (let index = 1; index <= ligne; index++) {
         let tr = document.createElement("tr"); //que pour les lignes
+        tr = this.data.value[1];
 
         for (let j = 1; j <= colonne; j++) {
           let td = document.createElement("td"); //pour les lignes suivantes dans un tr, colonne
